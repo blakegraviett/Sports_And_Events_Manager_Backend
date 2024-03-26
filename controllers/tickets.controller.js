b1b9f8b06b9c0907c76a712860deca41fbef84b7
Blake Graviett <blake.graviett@gmail.com>
2024-03-26 15:52:18 -0500

    qr code

--- controllers/tickets.controller.js
+++ controllers/tickets.controller.js
@@ -10,43 +10,34 @@ const Ticket = require('../models/ticket.model')
 const purchaseTickets = async (req, res) => {
   const event = req.body
   switch (event.type) {
-    case 'payment_intent.succeeded': {
-      // ! SEND EMAIL ONLY IF PAYMENT IS SUCCESSFUL
-      // make a id for the ticket
-      const ticketId = crypto.randomBytes(60).toString('hex')
+    case 'payment_intent.succeeded':
+      {
+        // ! SEND EMAIL ONLY IF PAYMENT IS SUCCESSFUL
+        // make a id for the ticket
+        const ticketId = crypto.randomBytes(60).toString('hex')
 
-      // qr code link
-      const origin = `https://www.sportalmanager.com/tickets/${ticketId}`
+        // qr code link
+        const origin = `https://www.sportalmanager.com/tickets/${ticketId}`
 
-      //.createRequestcode
-      const qrCode = await qr.imageSync(origin, {
-        errorCorrectionLevel: 'H',
-        type: 'png',
-      })
+        //.createRequestcode
+        const qrCode = await qr.toString(origin, {
+          errorCorrectionLevel: 'H',
+        })
 
-      // create the ticket
-      await Ticket.create({
-        ticketId,
-      })
+        // create the ticket
+        await Ticket.create({
+          ticketId,
+        })
 
-      // send email to the user
-      await sendEmail({
-        from: 'sportalmanager@gmail.com',
-        to: event['data']['object']['receipt_email'],
-        subject: 'Ticket Purchase 5',
-        html: `Hi,<br><br>You have successfully purchased a ticket.<br><br>Thank you for your purchase.<br><br>Regards,<br><br>Team Sportal.<br><br>`,
-        attachments: [
-          {
-            filename: 'qr_code.png',
-            content: qrCode,
-            encoding: 'base64',
-            cid: 'unique@nodemailer.com',
-          },
-        ],
-      })
-    }
-    default:
-      // Unexpected event type
+        // send email to the user
+        await sendEmail({
+          from: 'sportalmanager@gmail.com',
+          to: event['data']['object']['receipt_email'],
+          subject: 'Ticket Purchase 5',
+          html: `Hi,<br><br>You have successfully purchased a ticket.<br><br>Thank you for your purchase.<br><br>Regards,<br><br>Team Sportal.<br><br> <p>${qrCode}</p>`,
+        })
+      }
+
       return res.status(400).end()
   }
 }

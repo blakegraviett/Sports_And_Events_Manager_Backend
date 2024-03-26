4bc427a160ef2b667c5b11936c8262dce4898483
Blake Graviett <blake.graviett@gmail.com>
2024-03-26 15:39:24 -0500

    test

--- controllers/tickets.controller.js
+++ controllers/tickets.controller.js
@@ -33,7 +33,7 @@ const purchaseTickets = async (req, res) => {
       await sendEmail({
         from: 'sportalmanager@gmail.com',
         to: event['data']['object']['receipt_email'],
-        subject: 'Ticket Purchase 1',
+        subject: 'Ticket Purchase 5',
         html: `Hi,<br><br>You have successfully purchased a ticket.<br><br>Thank you for your purchase.<br><br>Regards,<br><br>Team Sportal.<br><br>`,
         attachments: [
           {
